4efca7d27b909e2edca172d1fc3efee365b9b769
Kavindu Dilshan <kavindu4543@gmail.com>
2025-04-11 14:00:40 +0530

    Email Sending Dervice completed

--- src/main/java/com/kavindu/Main.java
+++ src/main/java/com/kavindu/Main.java
@@ -3,9 +3,11 @@ package com.kavindu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
