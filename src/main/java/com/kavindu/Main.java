229e725ae69d78073902f607350566a3e21c0777
Kavindu Dilshan <kavindu4543@gmail.com>
2025-04-11 21:32:14 +0530

    Authentication Complteted

--- src/main/java/com/kavindu/Main.java
+++ src/main/java/com/kavindu/Main.java
@@ -1,7 +1,11 @@
 package com.kavindu;
 
+import com.kavindu.role.Role;
+import com.kavindu.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -11,5 +15,16 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
+    }
+
+    @Bean
+    public CommandLineRunner runner(RoleRepository roleRepository){
+        return args -> {
+            if (roleRepository.findByName("USER").isEmpty()){
+                roleRepository.save(
+                        Role.builder().name("USER").build()
+                );
+            }
+        };
     }
 }
