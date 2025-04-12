68348ebf08a0510f151420fae3e684463ad86700
Kavindu Dilshan <kavindu4543@gmail.com>
2025-04-12 11:48:14 +0530

    Entity creaction completed

---
+++ src/main/java/com/kavindu/history/BookTransactionHistory.java
@@ -0,0 +1,21 @@
+package com.kavindu.history;
+
+import com.kavindu.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    private boolean returned;
+    private boolean returnedApproved;
+}
